c3a372f5dac1f7b323394dd078fbb8cdeb13c046
nagymatekaroly <nagymatekaroly@gmail.com>
2025-03-01 10:55:03 +0100

    +page.server.js fix

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 export const load = async (event) => { 
-    console.log(event)
+    console.log(event.locals)
     return {
         user: event.locals.user //netlifyn e helyére a link kell 
     }
