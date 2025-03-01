8bc2f40cd5ad893dfa9a5e717b75c9737262d1e2
nagymatekaroly <nagymatekaroly@gmail.com>
2025-03-01 10:46:53 +0100

    netlify change

--- src/routes/+page.svelte.js
+++ src/routes/+page.svelte.js
@@ -1,6 +1,6 @@
 export const load = async (event) => { 
     console.log(event)
     return {
-        user: event.locals.user
+        user: event.locals.user //netlifyn e helyére a link kell 
     }
 }
