89410df592796f8a5a266f789eb4077cef0a2f03
nagymatekaroly <nagymatekaroly@gmail.com>
2025-03-01 12:39:23 +0100

    container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     const {data} = $props()
+    var x = $state(0)
 </script>
 
 <h1>Cica</h1>
@@ -12,3 +13,14 @@
     <button class="ui red button" formaction="?/cica">Elküld 1</button>
     <button class="ui blue button" formaction="?/kutya">Elküld 2</button>
 </form>
+<div clas="ui divider" ></div>
+<div class="slider">
+    <input type="range" min="0" max="100" bind:value={x}>
+<div class="ui card">{x}</div>
+</div>
+
+<button class="ui red small button"
+onclick={() => { fetch('/server',{
+    method: 'POST',
+    body: JSON.stringify({x, y:1})})
+}}>POST</button>
--- src/routes/server/+server.js
+++ src/routes/server/+server.js
@@ -2,3 +2,9 @@ export const GET = async ({ request }) => {
     console.log(request)
     return new Response(JSON.stringify({x: 1, y:2}))
 }
+
+export const POST= async ({ request }) => {
+    var data = await request.json();
+    console.log(data)
+    return new Response(JSON.stringify({x: 4, y:5}))
+}
